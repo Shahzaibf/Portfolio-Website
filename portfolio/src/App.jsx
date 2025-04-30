2de9f43342a256894a136df75aaf732257432720
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2025-04-30 13:22:41 -0400

    base

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import ProfileCard from './components/ProfileCard'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <ProfileCard/>
+    </>
+  )
+}
+
+export default App
